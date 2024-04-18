8898afd3dbbcf3502987a32fd0ae490b7055f1ce
Chetan Satpute <chetan.satpute2002@gmail.com>
2024-04-18 23:12:34 +0530

    log server requests in development environment

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,12 @@
 import express from 'express';
+import morgan from 'morgan';
+
+import {IS_DEVELOPMENT} from './env';
 
 const app = express();
+
+if (IS_DEVELOPMENT) {
+  app.use(morgan('dev'));
+}
 
 export default app;
--- src/env.ts
+++ src/env.ts
@@ -3,4 +3,7 @@ import dotenv from 'dotenv';
 // load environment variables
 dotenv.config();
 
-export const {PORT = 8080} = process.env;
+export const {PORT = 8080, NODE_ENV = 'development'} = process.env;
+
+export const IS_PRODUCTION = NODE_ENV === 'production';
+export const IS_DEVELOPMENT = NODE_ENV === 'development';
